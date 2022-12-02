e321477a65df2778f7f768bb92b29a6c3a4df2fd
Ethan Hawes <ethanhawes@outlook.com>
2022-12-02 11:48:02 +0000

    re-added 11ty config file
    This was missing when I pulled main. Added a working, commented eleventy config to work out-the-box

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+// importing a library to allow for easier date conversions (see line 14)
+const { DateTime } = require('luxon');
+
+module.exports = function (eleventyConfig) {
+  // allows css, assets, and CMS config files to be passed into /public
+  eleventyConfig.addPassthroughCopy('./src/css/**/*.css');
+  eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/admin');
+
+  // watch CSS files for changes - doesn't trigger 11ty rebuild
+  eleventyConfig.setBrowserSyncConfig({
+    files: './public/css/**/*.css',
+  });
+
+  // normally, 11ty will render dates on blog posts in full JSDate format (Fri Dec 02 18:00:00 GMT-0600). That's ugly
+  // this filter allows dates to be converted into a normal, locale format. view the docs to learn more (https://moment.github.io/luxon/api-docs/index.html#datetime)
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      output: 'public',
+    },
+    // allows .html files to contain nunjucks templating language
+    htmlTemplateEngine: 'njk',
+  };
+};
