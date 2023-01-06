66f308bb3dac01c39d52659bc2ce104ccbf575a8
monkeywhovapes <50428613+monkeywhovapes@users.noreply.github.com>
2023-01-06 14:16:48 -0500

    click bg to click out of mobile menu & minify js

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -16,3 +16,22 @@ hamburgerMenu.addEventListener('click', function() {
         navbarMenu.classList.remove("open");
     }
 });
+
+// .open:before selection to close the menu when open and not the ul section
+document.getElementsByTagName('nav')[0].addEventListener('click', backgroundClose);
+    function backgroundClose(event) {
+        console.log(event);
+        if (event.target.id !== 'navbar-menu'){
+            return
+        }
+        const isNavOpen = navbarMenu.classList.contains("open");
+        if (!isNavOpen) {
+            hamburgerMenu.setAttribute("aria-expanded", true);
+            hamburgerMenu.classList.add("clicked");
+            navbarMenu.classList.add("open");
+        } else {
+            hamburgerMenu.setAttribute("aria-expanded", false);
+            hamburgerMenu.classList.remove("clicked");
+            navbarMenu.classList.remove("open");
+        }
+    }
