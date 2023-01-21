4135de8dad9acb457aa47bea82b91c7d569bd6ac
monkeywhovapes <50428613+monkeywhovapes@users.noreply.github.com>
2023-01-20 23:59:56 -0500

    adding To the Top btn

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -35,3 +35,18 @@ document.getElementsByTagName('nav')[0].addEventListener('click', backgroundClos
             navbarMenu.classList.remove("open");
         }
     }
+    // Get the button
+    let mybutton = document.getElementById("toTopBtn");
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+        } else {
+        mybutton.style.display = "none";
+        }
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    }
